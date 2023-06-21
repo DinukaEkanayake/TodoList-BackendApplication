914d84c10e7d0209f3bd4cc5525603348d2b053e
Dinuka Ekanayake <dinukaekanayaka18@gail.com>
2023-06-21 22:42:39 +0530

    get todo-items by id and update json file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express') // importing the express framework and storin
 const fs=require('fs')
 const app = express()
 
+//get all todo items
 app.get('/todos',(request,response)=>{
 
     fs.readFile('./store/todos.json','utf-8',(err,data)=>{
@@ -16,6 +17,44 @@ app.get('/todos',(request,response)=>{
 
     })
 })
+
+//get todo items by id and update item
+app.get('/todos/:id/complete',(request,response)=>{
+
+    const id = request.params.id
+
+    const findTodosById = (todos,id) => {
+        
+        for (let i = 0; i < todos.length; i++) {
+            
+            if (todos[i].id=== parseInt(id)) {
+                return i
+            }
+        }
+
+        return -1
+    }
+
+    fs.readFile('./store/todos.json','utf-8',(err,data)=>{
+
+        if (err) {
+            return response.status(500).send('file not found')
+        }
+        let todos = JSON.parse(data)
+        const todoIndex = findTodosById(todos,id)
+
+        if (todoIndex===-1) {
+            return response.status(404).send('not found in the list')
+        }
+
+        todos[todoIndex].complete = true
+        fs.writeFile('./store/todos.json',JSON.stringify(todos),()=>{
+            response.json({'status':'ok'})
+        })
+
+    })
+})
+
 
 
 app.listen(3000, ()=>{
